b2cf09fa888aa5f8fff82f52d0cbc166aaca053b
Jumaruba <juliane.marubayashi@gmail.com>
2020-10-28 19:13:44 +0000

    form in the center

---
+++ src/lib/screens/signup/label.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import "../../style.dart";
+import "../../ui_elements.dart";
+
+class Label extends StatelessWidget {
+
+  String _label;
+  Label(this._label);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: Text(
+        _label,
+        style: mediumText,
+    ),
+    margin: EdgeInsets.fromLTRB(0, 20, 150, 20)
+    );
+  }
+}
--- src/lib/screens/signup/signup.dart
+++ src/lib/screens/signup/signup.dart
@@ -1,30 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import "../../style.dart";
-import "field.dart";
-import "../../ui_elements.dart";
+import '../../style.dart';
+import "label.dart";
+import 'field.dart';
+import '../../ui_elements.dart';
 
-
-class SignupScreen extends StatelessWidget{
-  Widget build(BuildContext context){
+class SignupScreen extends StatelessWidget {
+  Widget build(BuildContext context) {
     return Scaffold(
         appBar: appBar,
         bottomNavigationBar: navigationBar,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Container(
-            decoration: BoxDecoration(color: Colors.red),
-            child: Text('helo')
-
-          ),
-
-
-        ],
-      )
-    );
+        body: Container(
+            width: double.infinity,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [new Label("name"), new Field("name", null)],
+            )));
   }
 }
-
-
