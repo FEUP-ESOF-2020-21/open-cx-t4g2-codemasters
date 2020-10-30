2402e6bb2a0f86a0d07ee9647a4ca0da45dec237
Jumaruba <juliane.marubayashi@gmail.com>
2020-10-30 11:44:50 +0000

    field and labels

--- src/lib/screens/utils/label.dart
+++ src/lib/screens/utils/label.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import "../../style.dart";
-import "../../ui_elements.dart";
+import '../../style.dart';
+import '../../ui_elements.dart';
 
 class Label extends StatelessWidget {
 
