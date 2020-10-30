656ca6f22cf72eaf531551b7a35b212042697bd9
TiagooGomess <tiagogomes2011@live.com.pt>
2020-10-30 13:45:57 +0000

    Added description

--- src/lib/widgets/feed/recommended_carousel.dart
+++ src/lib/widgets/feed/recommended_carousel.dart
@@ -2,19 +2,28 @@ import 'package:flutter/material.dart';
 import "../../style.dart";
 
 class RecommendedCarousel extends StatelessWidget {
-  final List<String> conferences = [
-    'ajsdfioa',
-    'dsovf',
-    'nddv',
-    'vdjnvdv',
-    'ajsdfioa',
-    'dsovf',
-    'nddv',
-    'vdjnvdv',
-    'ajsdfioa',
-    'dsovf',
-    'nddv',
-    'vdjnvdv'
+  final List<Map<String, String>> conferences = [
+    {
+      'title': 'title1',
+      'description':
+          'description1description1description1description1description1description1description1description1',
+    },
+    {
+      'title': 'title1',
+      'description':
+          'description2description2description2description2description2description2description2description2',
+    },
+    {'title': 'title2', 'description': 'description3'},
+    {'title': 'title3', 'description': 'description4'},
+    {'title': 'title4', 'description': 'description5'},
+    {'title': 'title5', 'description': 'description6'},
+    {'title': 'title6', 'description': 'description7'},
+    {'title': 'title7', 'description': 'description8'},
+    {'title': 'title8', 'description': 'description9'},
+    {'title': 'title9', 'description': 'description10'},
+    {'title': 'title10', 'description': 'description11'},
+    {'title': 'title11', 'description': 'description12'},
+    {'title': 'title12', 'description': 'description13'},
   ];
 
   @override
@@ -54,8 +63,8 @@ class RecommendedCarousel extends StatelessWidget {
     for (int i = 0; i < conferences.length; i++) {
       containers.add(
         GestureDetector(
-          onTap: () =>
-              print('Entered in the conference info: ' + conferences[i]),
+          onTap: () => print(
+              'Entered in the conference info: ' + conferences[i]['title']),
           child: Container(
             margin: EdgeInsets.all(10.0),
             height: 200,
@@ -70,10 +79,19 @@ class RecommendedCarousel extends StatelessWidget {
                 Image(
                   image: AssetImage('assets/images/conference_test.jpg'),
                 ),
-                SizedBox(height: 10),
+                SizedBox(height: 6),
                 Text(
-                  conferences[i],
+                  conferences[i]['title'],
                   style: smallerText,
+                ),
+                Text(
+                  conferences[i]['description'],
+                  overflow: TextOverflow
+                      .ellipsis, // assim aparecem '...' quando não cabe mais texto
+                  style: TextStyle(
+                    fontSize: 10,
+                  ),
+                  textAlign: TextAlign.center,
                 ),
               ],
             ),
