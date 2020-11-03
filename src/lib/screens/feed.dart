10d6818ff7571cb0a2ba1c9ae016869690fb1c1f
2dukes <ruipinto02@hotmail.com>
2020-11-03 12:57:08 +0000

    Filtered Feed By Top Rate and Coming Next.

--- src/lib/screens/feed.dart
+++ src/lib/screens/feed.dart
@@ -14,30 +14,24 @@ class FeedScreen extends StatefulWidget {
 }
 
 class _FeedScreenState extends State<FeedScreen> {
-  final List<Map<String, String>> conferences = [
-    // tratar disto no backend
-    {
-      'title': 'title0',
-      'description':
-          'descri ption1descri ption1de scription1descrip tion1descrip tion1 description1descr iption1description1'
-    },
-    {
-      'title': 'title1',
-      'description':
-          'description2descrip tion2des cription2 description2descripti on2descripti on2description 2description2'
-    },
-    {'title': 'title2', 'description': 'description3'},
-    {'title': 'title3', 'description': 'description4'},
-    {'title': 'title4', 'description': 'description5'},
-    {'title': 'title5', 'description': 'description6'},
-    {'title': 'title6', 'description': 'description7'},
-    {'title': 'title7', 'description': 'description8'},
-    {'title': 'title8', 'description': 'description9'},
-    {'title': 'title9', 'description': 'description10'},
-    {'title': 'title10', 'description': 'description11'},
-    {'title': 'title11', 'description': 'description12'},
-    {'title': 'title12', 'description': 'description13'},
-  ];
+  final int minRate = 4;
+
+  List<DocumentSnapshot> filterTopRate(
+      List<DocumentSnapshot> totalConferences) {
+    return totalConferences
+        .where((conference) => conference['rate'] > minRate)
+        .toList();
+  }
+
+  List<DocumentSnapshot> filterComingNext(
+      List<DocumentSnapshot> totalConferences) {
+    return totalConferences.where((conference) {
+      DateTime confTime = DateTime.fromMillisecondsSinceEpoch(
+          conference['date'].seconds * 1000);
+
+      return confTime.compareTo(DateTime.now()) > 0;
+    }).toList();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -59,12 +53,12 @@ class _FeedScreenState extends State<FeedScreen> {
                         .documents), // passar as conferencias ordenadas por recomendação
                     SizedBox(height: 20.0),
                     //TopRatedCarousel(),
-                    TopRatedCarousel(snapshot.data
-                        .documents), // passar as conferencias ordenadas por rating
+                    TopRatedCarousel(filterTopRate(snapshot.data
+                        .documents)), // passar as conferencias ordenadas por rating
                     SizedBox(height: 20.0),
                     //ComingNextCarousel(),
-                    ComingNextCarousel(snapshot.data
-                        .documents), // passar as conferencias ordenadas por data
+                    ComingNextCarousel(filterComingNext(snapshot.data
+                        .documents)), // passar as conferencias ordenadas por data
                   ],
                 ),
               );
