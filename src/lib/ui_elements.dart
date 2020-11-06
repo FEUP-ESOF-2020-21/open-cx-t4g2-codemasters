109fe9023611e944f2d59135f37df19aacadb87b
Jumaruba <juliane.marubayashi@gmail.com>
2020-11-06 13:36:10 +0000

    bottom bar working

--- src/lib/ui_elements.dart
+++ src/lib/ui_elements.dart
@@ -1,29 +1,66 @@
-import 'package:ESOF/style.dart';
+import 'package:ESOF/model/conference.dart';
+import 'package:ESOF/screens/login.dart';
+import 'package:ESOF/screens/feed.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:ESOF/screens/signup.dart';
 import 'package:flutter/material.dart';
+import 'model/speaker.dart';
+import 'package:ESOF/style.dart';
+import 'screens/post.dart';
+import 'package:flutter/material.dart';
+import 'screens/login.dart';
 
-final BottomNavigationBar navigationBar = BottomNavigationBar(
-  backgroundColor: accentOrange,
-  type: BottomNavigationBarType.fixed,
-  items: [
-    BottomNavigationBarItem(
-      icon:
-          Image.asset("assets/icons/1x/single-folded-content.png", scale: 1.5),
-      label: "Create post",
-    ),
-    BottomNavigationBarItem(
-      icon: Image.asset("assets/icons/1x/ic_home_48px.png", scale: 1.5),
-      label: "Home",
-    ),
-    BottomNavigationBarItem(
-      icon: Image.asset("assets/icons/1x/zoom-2.png", scale: 1.5),
-      label: "Explore",
-    ),
-    BottomNavigationBarItem(
-      icon: Image.asset("assets/icons/1x/single-01.png", scale: 1.5),
-      label: "Profile",
-    ),
-  ],
-);
+final pages = [
+  Center(child: Text('create post')),
+  FeedScreen(),
+  Center(child: Text('explore')),
+  Center(child: Text('profile'))
+];
+
+class MyHome extends StatefulWidget {
+  @override
+  _MyHomeState createState() => _MyHomeState();
+}
+
+class _MyHomeState extends State<MyHome> {
+  int _currentIndex = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: appBar,
+      body: pages[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+          backgroundColor: accentOrange,
+          type: BottomNavigationBarType.fixed,
+          currentIndex: _currentIndex,
+          items: [
+            BottomNavigationBarItem(
+              icon: Image.asset("assets/icons/1x/single-folded-content.png",
+                  scale: 1.5),
+              label: "Create post",
+            ),
+            BottomNavigationBarItem(
+              icon: Image.asset("assets/icons/1x/ic_home_48px.png", scale: 1.5),
+              label: "Home",
+            ),
+            BottomNavigationBarItem(
+              icon: Image.asset("assets/icons/1x/zoom-2.png", scale: 1.5),
+              label: "Explore",
+            ),
+            BottomNavigationBarItem(
+              icon: Image.asset("assets/icons/1x/single-01.png", scale: 1.5),
+              label: "Profile",
+            ),
+          ],
+          onTap: (index) {
+            setState(() {
+              _currentIndex = index;
+            });
+          }),
+    );
+  }
+}
 
 final AppBar appBar = AppBar(
   actions: [
