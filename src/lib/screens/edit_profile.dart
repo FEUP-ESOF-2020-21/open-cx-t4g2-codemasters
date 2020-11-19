9c6e961e56e18f3f81ffa898df59a0d9b4803d70
TiagooGomess <tiagogomes2011@live.com.pt>
2020-11-19 09:46:29 +0000

    Added a form

--- src/lib/screens/edit_profile.dart
+++ src/lib/screens/edit_profile.dart
@@ -12,6 +12,7 @@ class EditProfileScreen extends StatefulWidget {
 }
 
 class _EditProfileScreenState extends State<EditProfileScreen> {
+  final _formKey = GlobalKey<FormState>();
   Icon _username = Icon(Icons.people, color: Colors.black54);
   Icon _email = Icon(Icons.alternate_email, color: Colors.black54);
   Icon _bio = Icon(Icons.description, color: Colors.black54);
@@ -20,86 +21,129 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: ListView(
-          padding: EdgeInsets.symmetric(vertical: 85.0),
-          children: <Widget>[
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Text(
-                  'Edit Profile',
-                  style: bigText,
-                ),
-              ],
-            ),
-            SizedBox(
-              height: 20.0,
-            ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Label("New Photo:"),
-                SizedBox(height: 20),
-                _image,
-                SizedBox(height: 20),
-                Label("New Name:"),
-                Field(
-                  hintTxt: "New Name",
-                  icon: _username,
-                  validator: (String value) {
-                    if (value.isEmpty) return '    Please enter your new Name';
-                    return null;
-                  },
-                ),
-                Label("New Email:"),
-                Field(
-                  hintTxt: "New Email",
-                  icon: _email,
-                  validator: (String value) {
-                    if (value.isEmpty) return '    Please enter your new Email';
-                    String msg = validateEmail(value);
-                    if (msg != null) return '    ' + msg;
-                  },
-                ),
-                Label("New Bio:"),
-                Field(
-                  hintTxt: "New Bio",
-                  icon: _bio,
-                  maxLines: 10,
-                  validator: (String value) {
-                    if (value.isEmpty) return '    Please enter your new Bio';
-                    return null;
-                  },
-                ),
-                Label("New Password:"),
-                Field(
-                  hintTxt: "New Password",
-                  isPassword: true,
-                  icon: _key,
-                  validator: (String value) {
-                    if (value.isEmpty)
-                      return '    Please enter your new Password';
-                    if (value.length < 7)
-                      return '    Password should be minimum 7 characters';
-                  },
-                ),
-                SizedBox(height: 60),
-                FlatButton(
-                  onPressed: () => print('Submit...'),
-                  padding: EdgeInsets.fromLTRB(100, 20, 100, 20),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20.0),
+      body: Form(
+        key: _formKey,
+        child: SafeArea(
+          child: ListView(
+            padding: EdgeInsets.symmetric(vertical: 85.0),
+            children: <Widget>[
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    'Edit Profile',
+                    style: bigText,
                   ),
-                  color: Colors.orangeAccent,
-                  child: Text(
-                    'Submit',
-                    style: submitTextWhite,
+                ],
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Label("New Photo:"),
+                  SizedBox(height: 20),
+                  _image,
+                  SizedBox(height: 20),
+                  Label("New Name:"),
+                  Field(
+                    hintTxt: "New Name",
+                    icon: _username,
+                    validator: (String value) {
+                      if (value.isEmpty)
+                        return '    Please enter your new Name';
+                      _formKey.currentState.save();
+                      return null;
+                    },
+                    onSaved: (String value) {
+                      //user.name = value;
+                    },
                   ),
-                ),
-              ],
-            ),
-          ],
+                  Label("New Email:"),
+                  Field(
+                    hintTxt: "New Email",
+                    icon: _email,
+                    validator: (String value) {
+                      if (value.isEmpty)
+                        return '    Please enter your new Email';
+                      String msg = validateEmail(value);
+                      if (msg != null) return '    ' + msg;
+                      _formKey.currentState.save();
+                      return null;
+                    },
+                    onSaved: (String value) {
+                      //user.email = value;
+                    },
+                  ),
+                  Label("New Bio:"),
+                  Field(
+                    hintTxt: "New Bio",
+                    icon: _bio,
+                    maxLines: 10,
+                    validator: (String value) {
+                      if (value.isEmpty) return '    Please enter your new Bio';
+                      _formKey.currentState.save();
+                      return null;
+                    },
+                    onSaved: (String value) {
+                      //user.bio = value;
+                    },
+                  ),
+                  Label("New Password:"),
+                  Field(
+                    hintTxt: "New Password",
+                    isPassword: true,
+                    icon: _key,
+                    validator: (String value) {
+                      if (value.isEmpty)
+                        return '    Please enter your new Password';
+                      if (value.length < 7)
+                        return '    Password should be minimum 7 characters';
+                      _formKey.currentState.save();
+                      return null;
+                    },
+                    onSaved: (String value) {
+                      //user.password = value;
+                    },
+                  ),
+                  SizedBox(height: 60),
+                  FlatButton(
+                    onPressed: () async {
+                      print('Submit...');
+                      if (_formKey.currentState.validate()) {
+                        _formKey.currentState.save();
+                        // print(user.email);
+
+                        //String result =
+                        //await AuthService.signIn(user.email, user.password);
+                        // print(AuthService.auth.currentUser.uid);
+                        //if (result == null) {
+                        //Navigator.push(
+                        //  context, MaterialPageRoute(builder: (context) => MyHome()));
+                        //} else {
+                        //setState(() {
+                        //loginFailed = true;
+                        //loginFailedMsg = result;
+                        //});
+                        //}
+                        //}
+                      }
+                    },
+                    padding: EdgeInsets.fromLTRB(100, 20, 100, 20),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20.0),
+                    ),
+                    color: Colors.orangeAccent,
+                    child: Text(
+                      'Submit',
+                      style: submitTextWhite,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
       resizeToAvoidBottomPadding: false,
