f6df22ba24d91dec4d8b77ecacf6f4da67d5a764
TiagooGomess <tiagogomes2011@live.com.pt>
2020-11-28 18:11:46 +0000

    Finished add comment screen

--- src/lib/screens/add_comment.dart
+++ src/lib/screens/add_comment.dart
@@ -1,3 +1,5 @@
+import 'package:ESOF/screens/utils/field.dart';
+import 'package:ESOF/screens/utils/label.dart';
 import 'package:ESOF/style.dart';
 import 'package:flutter/material.dart';
 
@@ -7,17 +9,52 @@ class AddCommentScreen extends StatefulWidget {
 }
 
 class _AddCommentScreenState extends State<AddCommentScreen> {
+  final _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: ListView(
-          padding: EdgeInsets.symmetric(horizontal: 30, vertical: 250),
-          children: [
-            Text(
-              'Add Comment:',
-              style: bigText,
-            ),
-          ]),
+        padding: EdgeInsets.symmetric(horizontal: 30, vertical: 250),
+        children: [
+          Column(
+            children: [
+              Text(
+                'Comment:',
+                style: bigText,
+              ),
+              SizedBox(height: 50),
+              Field(
+                height: 90,
+                hintTxt: "\nComment",
+                icon: Icon(Icons.comment, color: Colors.black54),
+                maxLines: 20,
+                validator: (String value) {
+                  _formKey.currentState.save();
+                },
+                onSaved: (String value) {},
+              ),
+              SizedBox(height: 40),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  FlatButton(
+                    padding: EdgeInsets.fromLTRB(20, 20, 20, 20),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20.0),
+                    ),
+                    color: Colors.orangeAccent,
+                    onPressed: () => Navigator.of(context).pop(),
+                    child: Text(
+                      'Leave Comment',
+                      style: submitTextWhite,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
