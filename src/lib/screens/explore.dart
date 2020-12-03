2fd2f80872bdbae521444d1f71b39a8113530c67
Ricardo Nunes <ricardo.nunes.1892@gmail.com>
2020-12-03 20:40:24 +0000

    Suggested fixes made

--- src/lib/screens/explore.dart
+++ src/lib/screens/explore.dart
@@ -1,5 +1,6 @@
 import 'package:ESOF/model/conference.dart';
 import 'package:ESOF/screens/post.dart';
+import 'package:ESOF/widgets/common/feed_common.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
@@ -26,10 +27,13 @@ class ExploreScreen extends StatelessWidget {
 class ConferenceSearch extends SearchDelegate<String> {
   // If the conference has image return the url, otherwise returns a default.
   // Mudar para ficar mais modular!
-  hasImage(conference) {
-    return conference['img'] == null
-        ? 'http://www.theides.org/img/about.jpg'
-        : conference['img'];
+
+  bool _checkIfQueryContains(DocumentSnapshot doc, String keyword) {
+    return doc
+        .data()[keyword]
+        .toString()
+        .toLowerCase()
+        .contains(query.toLowerCase());
   }
 
   @override
@@ -55,11 +59,8 @@ class ConferenceSearch extends SearchDelegate<String> {
         });
   }
 
-
   @override
-  Widget buildResults(BuildContext context) {
-
-  }
+  Widget buildResults(BuildContext context) {}
 
   @override
   Widget buildSuggestions(BuildContext context) {
@@ -68,8 +69,13 @@ class ConferenceSearch extends SearchDelegate<String> {
       builder: (context, snapshot) {
         if (!snapshot.hasData) return new Text('Loading...');
         if (query == '') return new Text('');
-        final results = snapshot.data.documents
-            .where((a) => a.data()['title'].toString().contains(query));
+
+        var results = snapshot.data.documents.where((a) =>
+            _checkIfQueryContains(a, 'title') ||
+            _checkIfQueryContains(a, 'location') ||
+            _checkIfQueryContains(a, 'tag'));
+
+
 
         return ListView(
           children: results
@@ -80,7 +86,7 @@ class ConferenceSearch extends SearchDelegate<String> {
                       Navigator.push(
                           context,
                           MaterialPageRoute(
-                            //mudar para novo contrutor
+                              //mudar para novo contrutor
                               builder: (context) => PostScreen(new Conference(
                                   hasImage(conference),
                                   conference['title'],
