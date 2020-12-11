373cefa3013ce882d16bee017614e84054fbb370
Ricardo <ricardo.nunes.1892@gmail.com>
2020-12-11 12:00:59 -0100

    Trying to run the test

--- src/test_driver/app_test.dart
+++ src/test_driver/app_test.dart
@@ -8,18 +8,19 @@ import 'steps/test_steps.dart';
 
 Future<void> main() {
   final config = FlutterTestConfiguration()
-    ..features = [Glob(r"test_driver/features/**.feature")]
+    ..features = [Glob(r"./test_driver/features/**.feature")]
     ..reporters = [
       ProgressReporter(),
       TestRunSummaryReporter(),
       JsonReporter(path: './report.json')
-    ] // you can include the "StdoutReporter()" without the message level parameter for verbose log information
+    ]
     ..stepDefinitions = [
+      Screen(),
       TapWidget(),
       Appears()]
 
     ..restartAppBetweenScenarios = true
-    ..targetAppPath = "test_driver/app.dart"
+    ..targetAppPath = "./test_driver/app.dart"
     ..exitAfterTestRun = true; // set to false if debugging to exit cleanly
   return GherkinRunner().execute(config);
 }
--- src/test_driver/steps/test_steps.dart
+++ src/test_driver/steps/test_steps.dart
@@ -2,7 +2,7 @@ import 'package:flutter_driver/flutter_driver.dart';
 import 'package:flutter_gherkin/flutter_gherkin.dart';
 import 'package:gherkin/gherkin.dart';
 
-class FeedScreen extends Given1WithWorld<String, FlutterWorld> {
+class Screen extends Given1WithWorld<String, FlutterWorld> {
   @override
   Future<void> executeStep(String key) async {
     final locator = find.byValueKey(key);
